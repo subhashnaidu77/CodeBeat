1a1217b0831c809b13e0ee3c346cb77ae067fe54
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2025-06-12 19:10:33 +0530

    Add files via upload

---
+++ Day4/script.js
@@ -0,0 +1,59 @@
+
+// hello();
+// var a = 5;
+// a = 10; 
+// var a = 15;
+    // console.log(a);
+
+// let a ;// declare a
+// let a = 5;// initialzations
+
+//     let a = 5 ;
+// let a =10;- // let a = 15; // redeclaration not allowed
+//     a=10;
+//     console.log(a);
+
+// const a =5 ;
+// const a= 10 ; 
+// a= 10 ;
+// console.log(a);
+//  var a = 5;
+// function hello(){
+  
+//    a= 10 ;
+//     console.log(a);
+// }
+// console.log(a);
+// hello();
+
+//scope
+// functional scope , global scope annd block scope 
+
+
+// let  a = 5;
+// function hello(){
+//   console.log(a);
+//    a= 10 ;
+//     console.log(a);
+// }
+// console.log(a);
+// hello();
+
+
+// const  b  = 5;
+// function hello(){
+  
+//   const   b= 10 ;
+//     console.log(b);
+// }
+// console.log(b);
+// hello();
+//block scope
+const b = 20
+{
+    const  b = 15;
+    console.log(b);
+}
+
+// console.log(b);
+
