f48fb6e5701a641e4beb377f7b70976ddbf3551c
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2025-06-16 20:25:24 +0530

    Add files via upload

---
+++ Day5/script.js
@@ -0,0 +1,66 @@
+let x = 5;
+let y = '5' ;
+// +,-,%,>,<,= *
+
+// == and === 
+// console.log(x == y); // true
+// console.log(x===y);
+
+//ternary operator 
+
+let a = 222;
+
+let b = a>=21?"yes":"no";
+// console.log(b)
+
+// function add(a, b) {
+//     return a + b;
+// }
+// console.log(add(10,20));
+
+const add = function(a,b){
+    return a+b;
+
+}
+// console.log(add(10,20));
+
+//named function expression 
+
+const sub= function subtract(a,b){
+    return a-b;
+};
+// console.log(sub(30,19))
+
+// arrow func 
+const  mul =(a,b)=>{
+    return a*b;
+}
+// console.log(mul(10, 20));
+//arrow function with explicit return 
+const hello =(name)=>{
+    return `hi${name}`
+}
+
+// console.log(hello('subhash'));
+
+const  arr = [ 1, 2, 4 , 5];
+// console.log(arr[1]);
+// console.log(arr.length);
+// arr.pop(); // removed 5 here 
+// arr.push(10);// added 10 at the end 
+// console.log(arr);
+// shif t and unshift   to remove and add at  begining 
+
+// console.log(arr.slice(1,2))//
+// console.log(arr.splice(1,2))
+// console.log(arr.)
+
+
+// console.log(arr.includes(1)); // arr.findLastIndexOf(1);
+console.log(arr.find);
+
+
+
+// operators , datatypes , arrays , functions
+
+// objects , date methods using this date methodas we will create task 
