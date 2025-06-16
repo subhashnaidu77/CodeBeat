ef6a8899c0ab02b68a9e2c47e23e70d49eea0691
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2025-06-16 20:20:06 +0530

    Add files via upload

---
+++ Day6/script.js
@@ -0,0 +1,9 @@
+// set timeout and set interval ;
+// setTimeout(()=>{
+//     console.log("hello ")
+// },3000)
+// setTimeout(()=>{ block of code}, time );
+
+setInterval(()=>{
+    console.log("this is set inter val ")
+},2000)
